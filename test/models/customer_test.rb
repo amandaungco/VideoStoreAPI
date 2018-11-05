b79d47b2cb414de13fe1c64a8c24d240d0d3c1d8
Amanda Ungco <amanda.ungco@gmail.com>
2018-11-05 12:54:00 -0800

    customer_Test'

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:amanda) { customers(:Amanda)}
 
   it "must be valid" do
-    value(customer).must_be :valid?
+    value(amanda).must_be :valid?
   end
 end
