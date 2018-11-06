ddea75df294382585b7f4b4ce91689908bfe0b47
Amanda Ungco <amanda.ungco@gmail.com>
2018-11-05 18:39:50 -0800

    added routes for checkin/checkout for rentals controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :customers, only: [:index]
   resources :movies, only: [:index, :show, :create]
+
+  post '/rentals/check_in', to: 'rentals#check_in' as: 'check_in'
+  post '/rentals/check_out', to: 'rentals#check_out' as: 'check_out'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
