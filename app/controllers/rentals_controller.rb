e17b166348e5a3c886fb8558eed42f08f43c8fb5
Amanda Ungco <amanda.ungco@gmail.com>
2018-11-05 18:36:36 -0800

    updated db

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,13 +1,24 @@
 class RentalsController < ApplicationController
-# find customer
-# find movie
+# find customer before check in
+# find movie before check in
   def check_out
-    #check inventory of
-    #create new instance of rental
-    #checkout_date: Today
-    #The rental's check-out date should be set to today, and the due date should be set to a week from today.
+    #check inventory of movie_id
+    #call inventory method for checkout
+      #if positive
+        #create new instance of rental
+        #checkout_date: Today
+        #due_date: Today + 7
+        #check_in: nil
+      #else
+        #sorry we're out of stock
+        #bad_request
+      #end
   end
 
   def check_in
+    #find rental isntance for customer_id and movie_id
+    #update rental instance
+    #check_in: today
+    #call inventory method for check in
   end
 end
